de5fdd0ba0dec45e6976f667f0631c3ad316407f
hayes28 <heather.hayes918@gmail.com>
2024-02-10 15:40:06 -0600

    Can't have comments to meet the only 5 lines for task

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,6 +1,4 @@
-// 4. print string
 void main() {
   String str = "Holberton School";
-  // Concatenate the string three times, then add a newline, and append the first 9 characters of the string followed by a newline
   print(str * 3 + "\n" + str.substring(0, 9) + "\n");
 }
