d00c2cbd3b51888f1d78ab4fa61293bd1c8e3c91
hayes28 <heather.hayes918@gmail.com>
2024-02-17 12:22:02 -0600

    Task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,18 @@
+class Password {
+  String _password = '';
+  Password({required String password}) : _password = password;
+    String get password => _password;
+    
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) return false;
+    if (!password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!password.contains(RegExp(r'[a-z]'))) return false;
+    if (!password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $password";
+  }
+}
