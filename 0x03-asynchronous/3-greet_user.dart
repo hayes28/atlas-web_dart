51bb464410d2ea77c2ac3e29305243ce44f1ff84
hayes28 <heather.hayes918@gmail.com>
2024-02-21 16:14:36 -0600

    Task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,28 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userMap = json.decode(userData);
+    String username = userMap['username'];
+    return 'Hello, $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool credentials = await checkCredentials();
+    if (credentials == true) {
+      print('There is a user: $credentials');
+      return await greetUser();
+    } else {
+      print('There is a user: $credentials');
+      return 'Wrong credentials';
+      }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
