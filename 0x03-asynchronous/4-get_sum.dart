479ffbd6a8b26064616c24ba89013d825e94a7b6
hayes28 <heather.hayes918@gmail.com>
2024-02-21 16:26:12 -0600

    Task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,20 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userMap = json.decode(userData);
+    String id = userMap['id'];
+    String orders = await fetchUserOrders(id);
+    List<dynamic> orderList = json.decode(orders);
+    double total = 0;
+    for (var order in orderList) {
+      String price = await fetchProductPrice(order);
+      total += json.decode(price);
+    }
+    return total;
+  } catch (e) {
+    return 0;
+  }
+}
